85d2c8e7634354b587a9758ff0f657298f408b7e
TylerFord <tford5@hawk.iit.edu>
2023-03-08 19:04:24 -0600

    Initial Commit

---
+++ Schrodinger Differential/Schrodinger_DifferentialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Schrodinger_DifferentialApp.swift
+//  Schrodinger Differential
+//
+//  Created by IIT phys 440 on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Schrodinger_DifferentialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
